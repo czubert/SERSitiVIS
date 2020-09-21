a8038dd5e69f9a77a79a95f72fb05e4d076575a0
czubert <czubercik@gmail.com>
2020-09-21 22:33:45 +0200

    Mean spectrum option added - working Added plot with original spectra + baseline

--- visualisation/custom_plot.py
+++ visualisation/custom_plot.py
@@ -6,6 +6,8 @@ from . import draw
 from processing import utils
 
 SINGLE = 'Single spectra'
+AV = 'Average'
+BS = 'Baseline'
 MS = 'Mean spectrum'
 GS = 'Grouped spectra'
 RS = 'Raman Shift'
@@ -25,8 +27,8 @@ def show_plot(template, df, display_options_radio, key):
     """
     if display_options_radio == SINGLE:
         for col in range(len(df.columns)):
-            df3 = df.copy()
-            df3.reset_index(inplace=True)
+            df2 = df.copy()
+            df2.reset_index(inplace=True)
 
             deg = st.slider(f'{DEG} plot nr: {col}', min_value=1, max_value=20, value=5)
 
@@ -43,25 +45,45 @@ def show_plot(template, df, display_options_radio, key):
             #
             #     st.write(draw.draw_plot(df2.iloc[:, [0, col + 1]], y_value='Custom'))
 
-            df2 = utils.correct_baseline(df.copy(), deg)
-            df2.reset_index(inplace=True)
+            df3 = utils.correct_baseline(df.copy(), deg)
+            df3.reset_index(inplace=True)
 
-            st.write(draw.draw_plot(template, df2.iloc[:, [0, col + 1]], y_value='Custom'))
+            st.write(draw.draw_plot(template, df3.iloc[:, [0, col + 1]], y_value='Custom'))
 
 
     elif display_options_radio == MS:
-        df['Average'] = df.mean(axis=1)
+        import plotly.graph_objects as go
+        # getting mean values for each raman shift
         df2 = df.copy()
-        df2.reset_index(inplace=True)
-        st.write(draw.draw_plot(template, df2, y_value=MS))
-        utils.show_dataframe(df2, key)
+        df2[AV] = df2.mean(axis=1)
+        # df2.reset_index(inplace=True)
+        df2 = df2.loc[:, [AV]]
+
+        # getting baseline for mean spectra
+        deg = st.slider(f'{DEG}', min_value=1, max_value=20, value=5)
+
+        df2['base_line'] = peakutils.baseline(df2.loc[:, AV], deg)
+
+        fig = draw.draw_plot(template, df2, y_value=MS)
+        fig.add_traces([go.Scatter(y=df2[AV], name=MS)])
+        fig.add_traces([go.Scatter(y=df2['base_line'], name=BS)])
+        fig = draw.fig_layout(template, fig, 'Original spectra + baseline')
+        st.write(fig)
+
+        fig2 = draw.draw_plot(template, utils.correct_baseline(df2,deg), y_value=MS)
+        fig2 = draw.fig_layout(template, fig2, 'Spectra after baseline correction')
+        st.write(fig2)
+
 
     elif display_options_radio == GS:
         # changing columns names, so they are separated on the plot,
         # before all columns had the same name
         df.columns = np.arange(len(df.columns))
+
+        deg = st.slider(f'{DEG}', min_value=1, max_value=20, value=5)
         # drawing the plot
-        st.write(draw.draw_plot(template, df, y_value=GS))
+        st.write(draw.draw_plot(template, utils.correct_baseline(df, deg=deg), y_value=GS))
+        # st.write(draw.draw_plot(template, df, y_value=GS))
         utils.show_dataframe(df, key)
 
 
--- visualisation/draw.py
+++ visualisation/draw.py
@@ -4,8 +4,9 @@ import plotly.graph_objects as go
 import streamlit as st
 import pandas as pd
 
+RS = 'Raman Shift'
+DS = 'Dark Subtracted #1'
 
-@st.cache
 def draw_plot(template, df_for_plot, descr='Chosen spectra', y_value='Custom'):
     """
     # dictionary of tuples of dfs separated corresponding to the type of substrate
@@ -14,8 +15,7 @@ def draw_plot(template, df_for_plot, descr='Chosen spectra', y_value='Custom'):
     :param y_value: String
     :return: Plotly object - Figure
     """
-    RS = 'Raman Shift'
-    DS = 'Dark Subtracted #1'
+
 
     if y_value == 'Custom' or y_value == 'all':
         fig = px.line(df_for_plot, x=RS, y=DS, width=950, height=650,
@@ -23,8 +23,7 @@ def draw_plot(template, df_for_plot, descr='Chosen spectra', y_value='Custom'):
                       color_discrete_sequence=px.colors.qualitative.Alphabet)
 
     elif y_value == 'Mean spectrum':
-        st.write('asdasdqweqwe')
-        fig = px.line(df_for_plot, x=RS, y=DS, width=950, height=650,
+        fig = px.line(df_for_plot, y="Average", width=950, height=650,
                       # color_discrete_sequence=px.colors.qualitative.Alphabet, error_y='Average_err')
                       color_discrete_sequence=px.colors.qualitative.Alphabet)
 
@@ -39,6 +38,31 @@ def draw_plot(template, df_for_plot, descr='Chosen spectra', y_value='Custom'):
                       xaxis_title=f"{RS} [cm^-1]",
                       yaxis_title="Intensity",
                       title_font_size=20,
+                      legend=go.layout.Legend(x=0.5, y=-0.2, traceorder="normal",
+                                              font=dict(
+                                                  family="sans-serif",
+                                                  size=10,
+                                                  color="black"
+                                              ),
+                                              bgcolor="#fff",
+                                              bordercolor="#ccc",
+                                              borderwidth=0.4,
+                                              orientation='h',
+                                              xanchor='auto',
+                                              itemclick='toggle',
+
+                                              ))
+
+    return fig
+
+
+def fig_layout(template, fig, descr='Chosen spectra'):
+    # changing layout and styles
+    fig.update_layout(title=descr, template=template, height=450,
+                      xaxis_title=f"{RS} [cm^-1]",
+                      yaxis_title="Intensity",
+                      title_font_size=20,
+                      width=950,
                       legend=go.layout.Legend(x=0.5, y=-0.2, traceorder="normal",
                                               font=dict(
                                                   family="sans-serif",
