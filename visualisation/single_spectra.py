0c88fe9fbc787feec42e57378ea5bae839bae841
Pawel <czubercik@gmail.com>
2021-04-29 13:38:06 +0200

    changed composition of showing sliders, now they are next to the plots

--- visualisation/single_spectra.py
+++ visualisation/single_spectra.py
@@ -29,7 +29,7 @@ OPT_S = "Optimised Spectrum"
 
 
 def show_single_plots(df, plots_color, template, spectra_conversion_type):
-    global plot_line, description, fig_single_all, corrected_df, df_visual
+    global plot_line, description, fig_single_all, corrected_df, df_visual, col2, col1
     df2 = df.copy()
     
     for col in range(len(df2.columns)):
@@ -52,53 +52,53 @@ def show_single_plots(df, plots_color, template, spectra_conversion_type):
         elif spectra_conversion_type == OPT or spectra_conversion_type == NORM:
             if spectra_conversion_type == NORM:
                 normalized_df2 = utils.normalize_spectra(df2, col)
-                
+        
                 spectra_to_show = pd.DataFrame(normalized_df2).dropna()
-            
+    
             plot_line = FLAT
             description = OPT_S
-
-            col1, col2 = st.beta_columns(2)
-            with col1:
-                deg = st.slider(f'{DEG} plot nr: {col}', min_value=0, max_value=20, value=5, key=f'{col}')
+    
+            col1, col2 = st.beta_columns((2, 1))
             with col2:
+                deg = st.slider(f'{DEG} plot nr: {col}', min_value=0, max_value=20, value=5, key=f'{col}')
                 window = st.slider(f'{WINDOW} plot nr: {col}', min_value=1, max_value=20, value=3, key=f'{col}')
-
+    
             spectra_to_show[BS] = peakutils.baseline(spectra_to_show[spectra_to_show.columns[0]], deg)
-
+    
             # Creating DataFrame with applied Baseline correction
             df_visual = utils.correct_baseline(spectra_to_show, deg, key=SINGLE, model=spectra_to_show.columns[0])
-
+    
             # Refining DataFrame to make spectra flattened
             df_visual = utils.smoothen_the_spectra(df_visual, window, key=SINGLE)
-
+    
             # Showing spectra before baseline correction + baseline function
             fig_single_all = go.Figure()
             draw.fig_layout(template, fig_single_all, plots_colorscale=plots_color,
                             descr=f'{ORG}, {BS}, and {FLAT} + {BS}')
-
+    
             specs = {'org': df_visual.columns[0], BS: BS, COR: COR, FLAT: FLAT}
-            
+    
             for spec in specs.keys():
                 if spec == FLAT:
                     name = f'{FLAT} + {BS} correction'
                 else:
                     name = specs[spec]
-                
+
                 fig_single_all = draw.add_traces(df_visual, fig_single_all,
                                                  x=RS, y=specs[spec], name=name)
-        
+
         fig_single_corr = draw.add_traces_single_spectra(df_visual, fig_single_corr, x=RS, y=plot_line,
                                                          name=df_visual.columns[0])
-        
+
         draw.fig_layout(template, fig_single_corr, plots_colorscale=plots_color, descr=description)
-        
-        if spectra_conversion_type == RAW:
-            st.write(fig_single_corr)
-        else:
-            st.write(fig_single_corr)
-            st.write(fig_single_all)
-        
+
+        with col1:
+            if spectra_conversion_type == RAW:
+                st.write(fig_single_corr)
+            else:
+                st.write(fig_single_corr)
+                st.write(fig_single_all)
+
         file_name = f'{df_visual.columns[0]}_{FLAT}_{BS}_correction'
-        
+
         save_read.save_adj_spectra_to_file(df_visual, file_name, key=f'{col}')
