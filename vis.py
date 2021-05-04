cd2cd6836d6059baae9415a3c99f8864c6e5ee0b
charzewski <l.charzewski@gmail.com>
2021-05-04 20:07:05 +0200

    show data structure in raw text format

--- vis.py
+++ vis.py
@@ -1,7 +1,9 @@
 import base64
+import re
 
 import pandas as pd
 import streamlit as st
+import streamlit.components.v1 as components
 
 from constants import LABELS
 from processing import bwtek
@@ -15,6 +17,16 @@ from visualisation import mean_spectra
 from visualisation import p3d_spectra
 from visualisation import single_spectra
 from visualisation import visualisation_options as vis_opt
+
+
+def example_data_html(spectrometer):
+    files = utils.load_example_files(LABELS[spectrometer])
+    text = files[0].read()
+    files[0].seek(0)
+    html = f'<div style="font-family: monospace"><p>{text}</p></div>'
+    html = re.sub(r'\n', r'<br>', html)
+    return html
+
 
 st.set_page_config(
     page_title="SERSitive.eu",
@@ -110,7 +122,7 @@ if files:
             shift = st.slider(LABELS['SHIFT'], 0.0, 1.0, 0.0, 0.1)
         else:
             shift = st.slider(LABELS['SHIFT'], 0, 30000, 0, 250)
-    
+
         data_vis_option[chart_type](df, plots_color, template, spectra_conversion_type, shift)
     # All the other conversion types are single therefore no need for shift spectra
     else:
@@ -135,37 +147,35 @@ else:
 
     # TODO needs improvements, shows data only if BWTEK is choosen
     with st.beta_expander('For BWTEK - upload raw data in *.txt format without any changes'):
-        if spectrometer == LABELS['BWTEK']:
-            files = utils.load_example_files(spectrometer)
-            df, bwtek_metadata = bwtek.read_bwtek(files)
-            st.markdown('### Original data consists of metadata')
-            st.write(bwtek_metadata)
-            st.markdown('### And data')
-            st.write(df)
+        st.markdown('### Original data consists of metadata and data')
+        html = example_data_html('BWTEK')
+        st.components.v1.html(html, height=200, scrolling=True)
 
     with st.beta_expander('For WITec Alpha300 R+, upload spectra in *.txt or *.csv format as follows:'):
-        st.write(pd.read_csv('data_examples/witec/WITec(7).csv'))
         st.write('First column is X axis i.e Raman Shift (name of column is not important here)')
         st.write('Other columns should be the data itself')
         st.markdown(f'<b>Name of column</b> will be displayed as a <b>name of a plot in the legend</b>',
                     unsafe_allow_html=True)
         st.markdown(f"<p style='color:red'><b>Important:</b> Do not duplicate names of the columns",
                     unsafe_allow_html=True)
+        html = example_data_html('WITEC')
+        st.components.v1.html(html, height=200, scrolling=True)
 
     with st.beta_expander('For Renishaw spectra upload raw data in *.txt or *.csv format as shown below:'):
-        st.write(pd.read_csv('data_examples/renishaw/renishaw(6).txt', header=None, sep='\t'))
         st.write('First column is X axis i.e Raman Shift (name of column is not important here)')
         st.write('Second column is Y axis, and should be the data itself')
         st.markdown(f'<b>Name of a file</b> will be displayed as a <b>name of a plot in the legend</b>',
                     unsafe_allow_html=True)
+        html = example_data_html('RENI')
+        st.components.v1.html(html, height=200, scrolling=True)
 
     with st.beta_expander('For Wasatch spectra upload raw data in *.txt or *.csv:'):
-        st.write(pd.read_csv('data_examples/wasatch/SERSitive_next_day_1ppm-20201009-093810-270034-WP-00702.csv',
-                             header=None, sep='\t'))
+        html = example_data_html('WASATCH')
+        st.components.v1.html(html, height=200, scrolling=True)
 
     with st.beta_expander('For Teledyne Princeton Instruments spectra upload raw data in *.txt or *.csv:'):
-        st.write(pd.read_csv('data_examples/teledyne/teledyne(1).csv',
-                             header=None, sep=','))
+        html = example_data_html('TELEDYNE')
+        st.components.v1.html(html, height=200, scrolling=True)
 
     import authors
 
@@ -184,3 +194,4 @@ authors.made_by_pawel()
 authors.made_by_lukasz()
 
 print("Streamlit finish it's work")
+
