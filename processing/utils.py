c302083565babc6bd780e0e381f2633ef692cec0
Pawel <czubercik@gmail.com>
2021-05-13 23:17:00 +0200

    manual data works again

--- processing/utils.py
+++ processing/utils.py
@@ -236,13 +236,13 @@ def file_to_buffer(filepath):
 @st.cache(hash_funcs={io.StringIO: id})
 def load_example_files(spectrometer):
     examples = {
-        'BWTEK': ['./data_examples/bwtek/bwtek(2).txt', './data_examples/bwtek/bwtek(3).txt'],
-        'Renishaw': ['data_examples/renishaw/renishaw(5).txt', 'data_examples/renishaw/renishaw(6).txt'],
-        'WITec Alpha300 R+': ['data_examples/witec/WITec(5).csv', 'data_examples/witec/WITec(7).csv'],
-        'Wasatch System': ['data_examples/wasatch/SERSitive_next_day_1ppm-20201009-093810-270034-WP-00702.csv',
-                           'data_examples/wasatch/SERSitive_next_day_2ppm-20201009-093705-137238-WP-00702.csv'],
-        'Teledyne Princeton Instruments': ['data_examples/teledyne/teledyne(1).csv',
-                                           'data_examples/teledyne/teledyne(2).csv'],
+        'BWTEK': ['./data/data_examples/bwtek/bwtek(2).txt', './data/data_examples/bwtek/bwtek(3).txt'],
+        'Renishaw': ['data/data_examples/renishaw/renishaw(5).txt', 'data/data_examples/renishaw/renishaw(6).txt'],
+        'WITec Alpha300 R+': ['data/data_examples/witec/WITec(5).csv', 'data/data_examples/witec/WITec(7).csv'],
+        'Wasatch System': ['data/data_examples/wasatch/SERSitive_next_day_1ppm-20201009-093810-270034-WP-00702.csv',
+                           'data/data_examples/wasatch/SERSitive_next_day_2ppm-20201009-093705-137238-WP-00702.csv'],
+        'Teledyne Princeton Instruments': ['data/data_examples/teledyne/teledyne(1).csv',
+                                           'data/data_examples/teledyne/teledyne(2).csv'],
     }
 
     files = [file_to_buffer(f) for f in examples[spectrometer]]
