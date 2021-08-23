2103660a0756dc91dbbc19e4ca8b3ee53c270994
Pawel <czubercik@gmail.com>
2021-08-23 15:48:09 +0200

    witec should now work with multiple columns and single columns. if multiple than name of col is added to the name of the file

--- processing/witec.py
+++ processing/witec.py
@@ -21,19 +21,29 @@ def read_witec(uploaded_files, separator=','):
         uploaded_file.seek(0)
         single_uploaded_data_file = pd.DataFrame()
         data = utils.read_spec(uploaded_file, spectra_params)
-    
+
         single_uploaded_data_file[LABELS["RS"]] = data.iloc[:, 0]
-    
-        cols = {f'{uploaded_file.name[:-4]} ({col})': col for col in data.columns if not col.startswith('Unnamed') if
-                not col.startswith(LABELS['RS'])}
-    
+
+        if len(data.columns) == 2:
+            cols = {f'{uploaded_file.name[:-4]}': col for col in data.columns
+                    if not col.startswith('Unnamed')
+                    if not col.startswith(LABELS['RS'])}
+
+        elif len(data.columns) > 2:
+            cols = {f'{uploaded_file.name[:-4]} ({col})': col for col in data.columns
+                    if not col.startswith('Unnamed')
+                    if not col.startswith(LABELS['RS'])}
+        else:
+            st.warning('Something went wrong, please upload data one more time.')
+            st.stop()
+
         single_uploaded_data_file[list(cols.keys())] = data[list(cols.values())]
-    
+
         # cleaning data
         single_uploaded_data_file.dropna(inplace=True, how='any', axis=0)
         single_uploaded_data_file.dropna(inplace=True, axis=1)
         single_uploaded_data_file = single_uploaded_data_file.loc[:, ~single_uploaded_data_file.columns.duplicated()]
-    
+
         # setting Raman Shift as index
         single_uploaded_data_file.set_index(LABELS["RS"], inplace=True)
     
